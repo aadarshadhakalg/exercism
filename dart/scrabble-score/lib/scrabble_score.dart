4c1e095ab7452015330c40a92ff4bd6bdd4e3a29
Aadarsha Dhakal <braveboy2058@gmail.com>
2021-06-07 08:45:29 +0545

    Updated scrabble-score

--- dart/scrabble-score/lib/scrabble_score.dart
+++ dart/scrabble-score/lib/scrabble_score.dart
@@ -1,24 +1,31 @@
-Map<String, List> map = {
-  '1': ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-  '2': ['D', 'G'],
-  '3': ['B', 'C', 'M', 'P'],
-  '4': ['F', 'H', 'V', 'W', 'Y'],
-  '5': ['K'],
-  '8': ['J', 'X'],
-  '10': ['Q', 'J'],
+Map<String, int> map = {
+'a':1,
+'e':1,
+'i':1,
+'o':1,
+'u':1,
+'l':1,
+'n':1,
+'r':1,
+'s':1,
+'t':1,
+'d':2,
+'g':2,
+'b':3,
+'c':3,
+'m':3,
+'p':3,
+'f':4,
+'h':4,
+'v':4,
+'w':4,
+'y':4,
+'k':5,
+'j':8,
+'x':8,
+'q':10,
+'z':10,
 };
 
-int score(String data) {
-  int score = 0;
-  var upperData = data.toUpperCase().split('');
 
-  for (String input in upperData) {
-    for (String key in map.keys) {
-      if (map[key].contains(input)) {
-        score += int.parse(key);
-      }
-    }
-  }
-  
-  return score;
-}
+int score(String data) => data.toLowerCase().split('').fold(0, (previousValue, currentValue) => previousValue + map[currentValue] );
